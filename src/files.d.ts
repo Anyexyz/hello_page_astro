6d23d09f6ee4d0a7fa2f40001b924bf428d89016
Anyexyz <anyexyz@foxmail.com>
2024-05-27 20:13:43 +0800

    chore: Update Astro and rollup plugin versions

---
+++ src/files.d.ts
@@ -0,0 +1,4 @@
+declare module "./config/*.yml" {
+  const value: any;
+  export default value;
+}
---
+++ src/site.ts
@@ -0,0 +1,11 @@
+export const Site = {
+    title: "上网导航 - Hello！",
+    keywords: "导航页,Anye,Anyeの小站,百度搜索,哔哩哔哩搜索,知乎搜索,导航网站导航,上网导航",
+    description: "这是一个简单的导航页，用于快速访问常用网站。",
+    author: "Anye",
+    copyright: "https://anye.xyz",
+    icon: "https://cdn.anye.xyz/favicon.png",
+    lsvn: "1.0.0",
+    iconfont: "",
+    background: "https://cdn.anye.xyz/background.jpg",
+}
